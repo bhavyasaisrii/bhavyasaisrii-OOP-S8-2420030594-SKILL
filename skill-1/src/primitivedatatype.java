f6dfcd84ea62a2581c0a93b0069815dd91b5400d
bhavyasaisri2420030594 <2420030594@klh.edu.in>
2025-08-02 12:08:11 +0530

    Add files via upload

---
+++ skill-1/src/primitivedatatype.java
@@ -0,0 +1,29 @@
+
+public class primitivedatatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+			
+				// TODO Auto-generated method stub
+				byte age = 25;                         
+		        short currentYear = 2025;              
+		        int population = 1400000000;           
+		        long distanceToMoon = 384400000L;      
+		        float pi = 3.14f;                      
+		        double gravity = 9.80665;              
+		        char grade = 'A';                    
+		        boolean isJavaFun = true;            
+
+		        System.out.println("Age: " + age);
+		        System.out.println("Current Year: " + currentYear);
+		        System.out.println("Population: " + population);
+		        System.out.println("Distance to Moon: " + distanceToMoon + " meters");
+		        System.out.println("Value of Pi: " + pi);
+		        System.out.println("Gravity: " + gravity + " m/s^2");
+		        System.out.println("Grade: " + grade);
+		        System.out.println("Is Java fun to learn? " + isJavaFun);
+			}
+
+		}
---
+++ skill-1/src/studentdashboard.java
@@ -0,0 +1,23 @@
+
+public class studentdashboard {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				// TODO Auto-generated method stub
+				long rollNumber = 420030594;
+		        String name = "bhavya";
+		        float marks = 9.5f;
+		        char gender = 'F';
+		        boolean isPass = true;
+
+		       
+		        System.out.println("----- Student Summary Slip -----");
+		        System.out.println("Roll Number : " + rollNumber);
+		        System.out.println("Name        : " + name);
+		        System.out.println("Marks       : " + marks);
+		        System.out.println("Gender      : " + gender);
+		        System.out.println("Pass Status : " + (isPass ? "Pass" : "Fail"));
+			}
+
+		}
